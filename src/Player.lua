03ec3d1646e38f556227cfcc996671ce2cac5787
iblio <iblioce@gmail.com>
2025-05-03 15:17:15 +0200

    added rigth and left animation when moving

---
+++ src/Player.lua
@@ -0,0 +1,46 @@
+-- Player.lua
+anim8 = require("libs/anim8")
+love.graphics.setDefaultFilter("nearest", "nearest")
+
+local Player = {}
+Player.__index = Player
+
+function Player:new()
+    local self = setmetatable({}, Player)
+    self.direction = "up"
+    self.x = 0
+    self.y = 0
+    self.speed = 5
+    self.sprite = love.graphics.newImage("res/sprites/cake.png")
+    self.spriteSheet = love.graphics.newImage("res/sprites/pigMoving.png")
+    self.grid = anim8.newGrid(16, 16, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
+
+    self.animation = {}
+    self.animation.left = anim8.newAnimation(self.grid("1-4", 1), 0.2)
+    self.animation.right = anim8.newAnimation(self.grid("1-4", 2), 0.2)
+    return self
+end
+
+function Player:update(dt)
+    if love.keyboard.isDown("z") then self.y = self.y - self.speed end
+    if love.keyboard.isDown("s") then self.y = self.y + self.speed end
+    if love.keyboard.isDown("q") then self.x = self.x - self.speed 
+    self.animation.left:update(dt)
+    self.direction = "left"
+    end
+    if love.keyboard.isDown("d") then self.x = self.x + self.speed 
+    self.animation.right:update(dt)
+    self.direction = "right"
+    end
+end
+
+function Player:draw()
+    if self.direction == "left" then
+        self.animation.left:draw(self.spriteSheet, self.x, self.y, nil, 10)
+    else --self.direction == "right" then
+        self.animation.right:draw(self.spriteSheet, self.x, self.y, nil, 10)
+    end
+    
+end
+
+return Player
---
+++ src/main.lua
@@ -0,0 +1,23 @@
+_G.love = require("love")
+
+local PlayerClass = require("src.Player")
+local player
+local BackgroundClass = require("src.Background")
+local background
+
+function love.load()
+    player = PlayerClass:new()
+    background = BackgroundClass:new()
+end
+
+function love.update(dt)
+    player:update(dt)
+end
+
+function love.draw()
+    background:draw()
+    player:draw()
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+end
